585e114451b8defa0f73f033c227049f07e58a9f
cameron fang <cameronfang@cfang3.nomads.utk.edu>
2019-11-08 10:47:05 -0500

    Got rid of indices and changed the dataset in code to mtcars

--- app.R
+++ app.R
@@ -12,12 +12,6 @@ max.hp <- max(mtcars$hp)
 
 # Need a vector of axis variables as characters
 axis_vars <- names(mtcars)
-# Create a character vector of columns in mtcars 
-factors.indices <- vapply(mtcars, is.factor, TRUE) # vapply is a base R function like sapply & lapply that we haven't talked about
-# It applies a function (is.factor) to every element of a list (diamonds, remember that a data frame is a list)
-# The 'TRUE' is at the end to convey that we want vapply to return the results as a logical vector:
-#   I could just as well have used FALSE or c(TRUE, FALSE, TRUE) or any other logical vector
-factor.columns <- axis_vars[factor.indices]
 
 
 # Define UI for application that draws a histogram
@@ -49,11 +43,6 @@ ui <- fluidPage(
                   choices = axis_vars,
                   selected = "y"),
       
-      # I can't stop, I want to add a splash of color
-      selectInput(inputId = "color",
-                  label = "Color", 
-                  choices = factor.columns,
-                  selected = "hp"),
       
       actionButton("go", 
                    "Go!",
@@ -62,7 +51,7 @@ ui <- fluidPage(
     
     # Show a plot of diamonds data frame. This output doesn't care what that plot is, only that it will be associated with output$diamonds_plot
     mainPanel(
-      plotOutput("diamonds_plot")
+      plotOutput("mtcars_plot")
     )
   )
 )
@@ -73,30 +62,30 @@ server <- function(input, output) {
   # Filter diamonds based on carat - this doesn't strictly need to be in reactive,
   # ...but maybe we want to expand the app later, in which case it'll be useful to have it in reactive()
   filt_dia <- reactive({
-    diamonds %>%
-      filter(carat >= min(input$caratrange)) %>%
-      filter(carat <= max(input$caratrange))
+    mtcars %>%
+      filter(hp >= min(input$hprange)) %>%
+      filter(hp <= max(input$hprange))
   })
   
   # Make the plot
   # eventReactive listens for a change in state of input$go, and only runs the code when the state changes
   # Note use of aes_string, since input$xvar returns a string, not a reference to the object ("carat" not carat)
-  p_diamonds <- eventReactive(input$go, {
-    ggplot(filt_dia(), aes_string(x = input$xvar, y = input$yvar, colour = input$color)) + # Note that you need () after filt_dia, since filt_dia() is a function to get the object you want, not the actual object
+  p_mtcars <- eventReactive(input$go, {
+    ggplot(filt_dia(), aes_string(x = input$xvar, y = input$yvar)) + # Note that you need () after filt_dia, since filt_dia() is a function to get the object you want, not the actual object
       geom_point()
   })
   
   
   # Create diagnostic output window to show what kind of output the double slider creates
   output$diagnostic <- renderText(
-    input$caratrange
+    input$hprange
   )
   
   # Create a dynamic plot plot
   # I moved the ggplot into its own reactive context.
   # Note that creating a ggplot object is very fast - it is actually drawing the plot that is slow.
-  output$diamonds_plot <- renderPlot(
-    p_diamonds()
+  output$mtcars_plot <- renderPlot(
+    p_mtcars()
   )
   
 }
